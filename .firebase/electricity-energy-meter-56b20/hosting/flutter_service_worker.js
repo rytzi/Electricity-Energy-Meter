d588288a8e02c6e81c3bc0f4c6b3c1df93be7089
“Ritzi <ritziapril143@gmail.com>
2023-06-21 21:19:32 +0800

    final ig

---
+++ .firebase/electricity-energy-meter-56b20/hosting/flutter_service_worker.js
@@ -0,0 +1,206 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"index.html": "6b1a3ba7f6b707b0af4238c3b706f992",
+"/": "6b1a3ba7f6b707b0af4238c3b706f992",
+"version.json": "1ebf69cddbdf8b9c6953e1f9be1cb43c",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-192.png": "f9d186e319340a9aad96e10daefb6064",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "fd5fd27f15abe1858cf52cbc710af3b7",
+"assets/FontManifest.json": "87e04ddd47dc3c85e29dd1916b289700",
+"assets/NOTICES": "6066a75bdee309804bcfe88c1b1896f9",
+"assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
+"assets/AssetManifest.json": "6111ee543ca3bcb27648e55a6331c5e3",
+"assets/packages/flutterfire_ui/assets/fonts/SocialIcons.ttf": "a1207fae1578da27a062cb73d424aed9",
+"assets/packages/flutterfire_ui/assets/countries.json": "8c937aac9f3b69162be779fbcd6199d2",
+"assets/packages/flutterfire_ui/assets/icons/facebook.svg": "5fad3daafe7c7c5163fca56662d2738a",
+"assets/packages/flutterfire_ui/assets/icons/google.svg": "3b5ceaea5e2391782d39df225a375d5d",
+"assets/packages/flutterfire_ui/assets/icons/apple_light.svg": "2508cc7c5d302fd37edff1b87fedb594",
+"assets/packages/flutterfire_ui/assets/icons/twitter.svg": "6086e2aad26effea1344c8e118520e32",
+"assets/packages/flutterfire_ui/assets/icons/apple_dark.svg": "1b587ffd7d75c462847f8137a93f3161",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/AssetManifest.bin": "e55dce2627468a68a37cb29b2140c94c",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
+"canvaskit/chromium/canvaskit.wasm": "a1bdb8e068b45f18affefaf412122091",
+"canvaskit/chromium/canvaskit.js": "56facd8978a5292c9bb1d06dfcaee271",
+"canvaskit/canvaskit.wasm": "bcb5f27f1d4462c23499b5f5c73eadbf",
+"canvaskit/canvaskit.js": "8c9ab94b3e7d4f85ca01c1b0a1e54bd1",
+"canvaskit/skwasm.wasm": "d9d2a4b0bccb63bcedea89f95078411f",
+"canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "8262264cb58ee12f5b37d0c006991b0f"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -11,8 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final _db = FirebaseFirestore.instance;
+
   // Future<List<ESP32Model>> getESP32Details() async {
   //   final snapshot = await _db.collection("ESP32").get();
   //   final ESP32Data = snapshot.docs.map((e) => ESP32Model.fromSnapshot(e)).toList();
@@ -20,20 +20,28 @@ class _HomeScreenState extends State<HomeScreen> {
   // }
   Future<ESP32Model> getESP32Details() async {
     final snapshot = await _db.collection("ESP32").get();
-    final ESP32Data = snapshot.docs.map((e) => ESP32Model.fromSnapshot(e)).single;
+    final ESP32Data =
+        snapshot.docs.map((e) => ESP32Model.fromSnapshot(e)).single;
     return ESP32Data;
   }
 
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
+    int box;
+    if (screenSize.height >= screenSize.width) {
+      box = 2;
+    } else {
+      box = 4;
+    }
     return Scaffold(
       backgroundColor: Colors.white70,
       body: Stack(
         children: [
           Container(
             height: screenSize.height * .45,
-            decoration: BoxDecoration(color: Colors.black,
+            decoration: BoxDecoration(
+                color: Colors.black,
                 borderRadius: const BorderRadius.only(
                   bottomRight: Radius.circular(15.0),
                   bottomLeft: Radius.circular(15.0),
@@ -61,69 +69,125 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: GridView.count(
-                        crossAxisCount: 2,
+                        crossAxisCount: box,
                         childAspectRatio: .85,
                         crossAxisSpacing: 20,
                         mainAxisSpacing: 20,
                         children: [
                           StreamBuilder(
-                            stream: FirebaseFirestore.instance.collection('ESP32').snapshots(),
-                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-
-                              List<Map<String, dynamic>?>? ESP32Data = snapshot.data?.docs.map((e) => e.data() as Map<String, dynamic>?).toList();
+                            stream: FirebaseFirestore.instance
+                                .collection('ESP32')
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              List<Map<String, dynamic>?>? ESP32Data = snapshot
+                                  .data?.docs
+                                  .map((e) => e.data() as Map<String, dynamic>?)
+                                  .toList();
                               double _value = ESP32Data![0]?["VRMS"];
-
-                              if (!snapshot.hasData) {
+                              if (snapshot.hasData) {
+                                return GaugesWidget(
+                                  title: 'VRMS',
+                                  unit: "V",
+                                  value: _value,
+                                  minValue: 0,
+                                  maxValue: 500,
+                                  screenSize: screenSize,
+                                );
+                              } else {
                                 return Center(
-                                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.black)),
+                                  child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.black)),
                                 );
                               }
-                              return GaugesWidget(title: 'VRMS', unit: "V", value: _value, minValue: 0, maxValue: 500,);
                             },
                           ),
                           StreamBuilder(
-                            stream: FirebaseFirestore.instance.collection('ESP32').snapshots(),
-                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-
-                              List<Map<String, dynamic>?>? ESP32Data = snapshot.data?.docs.map((e) => e.data() as Map<String, dynamic>?).toList();
+                            stream: FirebaseFirestore.instance
+                                .collection('ESP32')
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              List<Map<String, dynamic>?>? ESP32Data = snapshot
+                                  .data?.docs
+                                  .map((e) => e.data() as Map<String, dynamic>?)
+                                  .toList();
                               double _value = ESP32Data![0]?["IRMS"];
-
-                              if (!snapshot.hasData) {
+                              if (snapshot.hasData) {
+                                return GaugesWidget(
+                                  title: 'IRMS',
+                                  unit: "A",
+                                  value: _value,
+                                  minValue: 0,
+                                  maxValue: 10,
+                                  screenSize: screenSize,
+                                );
+                              } else {
                                 return Center(
-                                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.black)),
+                                  child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.black)),
                                 );
                               }
-                              return GaugesWidget(title: 'IRMS', unit: "A", value: _value, minValue: 0, maxValue: 10,);
                             },
                           ),
                           StreamBuilder(
-                            stream: FirebaseFirestore.instance.collection('ESP32').snapshots(),
-                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-
-                              List<Map<String, dynamic>?>? ESP32Data = snapshot.data?.docs.map((e) => e.data() as Map<String, dynamic>?).toList();
+                            stream: FirebaseFirestore.instance
+                                .collection('ESP32')
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              List<Map<String, dynamic>?>? ESP32Data = snapshot
+                                  .data?.docs
+                                  .map((e) => e.data() as Map<String, dynamic>?)
+                                  .toList();
                               double _value = ESP32Data![0]?["Power"];
-
-                              if (!snapshot.hasData) {
+                              if (snapshot.hasData) {
+                                return GaugesWidget(
+                                  title: 'Power',
+                                  unit: "W",
+                                  value: _value,
+                                  minValue: 0,
+                                  maxValue: 50,
+                                  screenSize: screenSize,
+                                );
+                              } else {
                                 return Center(
-                                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.black)),
+                                  child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.black)),
                                 );
                               }
-                              return GaugesWidget(title: 'Power', unit: "W", value: _value, minValue: 0, maxValue: 50,);
                             },
                           ),
                           StreamBuilder(
-                            stream: FirebaseFirestore.instance.collection('ESP32').snapshots(),
-                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-
-                              List<Map<String, dynamic>?>? ESP32Data = snapshot.data?.docs.map((e) => e.data() as Map<String, dynamic>?).toList();
+                            stream: FirebaseFirestore.instance
+                                .collection('ESP32')
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<QuerySnapshot> snapshot) {
+                              List<Map<String, dynamic>?>? ESP32Data = snapshot
+                                  .data?.docs
+                                  .map((e) => e.data() as Map<String, dynamic>?)
+                                  .toList();
                               double _value = ESP32Data![0]?["KWH"];
-
-                              if (!snapshot.hasData) {
+                              if (snapshot.hasData) {
+                                return GaugesWidget(
+                                  title: 'KWH',
+                                  unit: "W",
+                                  value: _value,
+                                  minValue: 0,
+                                  maxValue: 10,
+                                  screenSize: screenSize,
+                                );
+                              } else {
                                 return Center(
-                                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.black)),
+                                  child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.black)),
                                 );
                               }
-                              return GaugesWidget(title: 'KWH', unit: "W", value: _value, minValue: 0, maxValue: 10,);
                             },
                           ),
                         ],
